980b788d5ac99fc01cc50ec5eff0344d00d590a3
lfpolk <47434013+lfpolk@users.noreply.github.com>
2021-07-07 12:41:37 -0500

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 
 const prodConfig = process.env.DATABASE_URL
 
-
+console.log("Prod Env: " + process.env.DATABASE_URL)
 const pool = new Pool({
     connectionString: process.env.NODE_ENV === "production" ? prodConfig : devConfig
 })
